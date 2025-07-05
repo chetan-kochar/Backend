c39b790d0a16706c5d160aca510eda16e81e84e1
chetan-kochar <chetankochar5976@gmail.com>
2025-07-06 01:41:33 +0530

    asyncHandler added to utils

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import dotenv from "dotenv";
 dotenv.config({path : "./.env"});
 
 import express from "express"
-const app = express()
+const app = express();
+
+const port = process.env.PORT || 3000
 
 // Connecting to dB
 import connect_dB from "./db/index.js"
 connect_dB()
+.then(()=>{
+    app.listen(port, ()=>{
+        console.log(`Listening on port ${port}/nhttp://localhost:${port}`)
+    })
+})
+.catch(err =>{console.log(err)});
+
+app.get("/",(req,res)=>{
+    res.send("Hello")
+})
