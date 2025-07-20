cd83cb9e0db23a6bf9ce06f98fc044fdb17c818a
chetan-kochar <chetankochar5976@gmail.com>
2025-07-20 18:33:43 +0530

    added routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { loginUser, logoutUser ,registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, logoutUser ,registerUser, refreshAccessToken, getCurrentUser, PassChange, updateEmail, updateFullName, updateAvatar, updatecoverImage } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import {verifyJwt} from "../middlewares/auth.middleware.js";
+
 
 
 const userRouter = Router();
@@ -29,5 +30,17 @@ userRouter.route("/logout").post(verifyJwt,logoutUser);
 
 
 userRouter.route("/refresh-AccessToken").post(refreshAccessToken);
+
+userRouter.route("/get-current-user",verifyJwt,getCurrentUser);
+
+userRouter.route("/change-password",verifyJwt,PassChange);
+
+userRouter.route("/update_email",verifyJwt,updateEmail);
+
+userRouter.route("/update_fullName",verifyJwt,updateFullName);
+
+userRouter.route("/update_avatar",verifyJwt,upload.single("avatar"),updateAvatar);
+
+userRouter.route("/update_cover_image",verifyJwt,upload.single("coverImage"),updatecoverImage);
 
 export {userRouter};
